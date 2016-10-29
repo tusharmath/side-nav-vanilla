3396d3060186ef3baf7115c45c8418bfbca31ed5
Tushar Mathur <tusharmath@gmail.com>
2016-10-29 17:27:49 +0530

    refactor(rwc): add global event types

--- rwc/rwc.ts
+++ rwc/rwc.ts
@@ -3,11 +3,15 @@ import { IAction } from "./IAction"
 import { IEffect } from "./IEffect"
 import { IEffectFunction } from "./IEffectFunction"
 
+export const RWC = {
+  INIT: "@@rwc/init"
+}
+
 export class ReactiveWebComponent extends HTMLElement implements IReactiveWebComponent {
   constructor(private runner: IEffectFunction) {
     super()
     this.dispatch = this.dispatch.bind(this)
-    runner("@@rwc/init")
+    runner(RWC.INIT).run(this)
   }
 
   dispatch(action: IAction) {
