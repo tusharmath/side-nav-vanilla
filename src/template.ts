60c1e44e815d01b9f812d0739bf83d291f3f6dff
Tushar Mathur <tusharmath@gmail.com>
2016-11-06 19:29:05 +0530

    smoothen animation

--- src/template.ts
+++ src/template.ts
@@ -7,7 +7,7 @@ export default `
       width: 100%;
       height: 100%;
       background-color: rgba(0, 0, 0, 0.6);
-      transition: background cubic-bezier(1, 0, 1, 1) 300ms;
+      transition: background cubic-bezier(0, 0, 0.3, 1) 300ms 300ms;
       cursor: pointer;
       user-select: none; 
     }
@@ -17,7 +17,7 @@ export default `
       width: 300px;
       box-shadow: 14px 0px 15px -7px rgba(0, 0, 0, 0.6);
       will-change: transform;
-      transition: transform cubic-bezier(1, 0, 1, 1) 300ms;
+      transition: transform cubic-bezier(0, 0, 0.3, 1) 300ms 300ms;
     }
     .no-anime {
       transition: none;
