1dc5f61c0d1841ff6d0c74b118c684f2bb0283f0
Tushar Mathur <tusharmath@gmail.com>
2016-11-15 17:48:57 +0530

    add horizontal nav

---
+++ src/components/HorizontalNav.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by tushar.mathur on 15/11/16.
+ */
+
+import {Dispatcher} from '../../rwc/Dispatcher'
+import {h} from 'preact'
+
+export const view = (d: Dispatcher<Event>) => h('div', {className: 'horizontal-nav'},
+  h('i', {className: 'material-icons', onClick: d.listener('show')}, 'menu')
+)
--- src/components/Main.ts
+++ src/components/Main.ts
@@ -1,7 +1,6 @@
 /**
  * Created by tushar.mathur on 04/11/16.
  */
-
 import {h} from 'preact'
 import * as O from 'observable-air'
 import * as R from 'ramda'
@@ -11,10 +10,8 @@ import MenuItems from './MenuItems'
 import {Model} from '../types/Model'
 import {Dispatcher, dispatcher} from '../../rwc/Dispatcher'
 import {Task} from '../../rwc/Task'
+import * as HorizontalNav from './HorizontalNav'
 
-const HorizontalNav = (d: Dispatcher<Event>) => h('div', {className: 'horizontal-nav'},
-  h('i', {className: 'material-icons', onClick: d.listener('show')}, 'menu')
-)
 export const view = (d: Dispatcher<Event>, model: Model, menuItems: JSX.Element, horizontalNav: JSX.Element) => {
   return h('div', null,
     horizontalNav,
@@ -26,7 +23,7 @@ export function main (): O.IObservable<Task> {
   const snReducer$ = SideNav.update(snDispatcher)
   const model$ = O.scan((f, v) => R.assoc('sideNav', f(v.sideNav), v), {sideNav: SideNav.init()}, snReducer$)
   const menuItemsView = MenuItems.view(snDispatcher)
-  const horizontalNavView = HorizontalNav(snDispatcher)
+  const horizontalNavView = HorizontalNav.view(snDispatcher)
 
   return O.map<Model, Task>(model => t.dom(view(snDispatcher, model, menuItemsView, horizontalNavView)), model$)
 }
